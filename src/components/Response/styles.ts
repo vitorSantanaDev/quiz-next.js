4f0e58fd5ee821bc691969fa88eb78b2bc811764
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-12 20:46:53 -0300

    💄 Styling the Response component

--- src/components/Response/styles.ts
+++ src/components/Response/styles.ts
@@ -1,11 +1,52 @@
 import styled from "styled-components";
 
-export const ResponseWrapper = styled.div `
+export const ResponseWrapper = styled.div`
+  width: 80%;
+  min-width: 500px;
   display: flex;
-`
+  align-self: stretch;
+  height: 100px;
+  margin: 16px;
+`;
 
-export const ContentWrapper = styled.div ``
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  position: relative;
+`;
 
-export const FrontContent = styled.div``
+export const FrontContent = styled.div`
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  border-radius: 12px;
+  padding: 16px;
+  height: 100%;
+  width: 100%;
+  position: absolute;
 
-export const BackContent = styled.div``
+  .letter {
+    font-size: 1.3rem;
+    font-weight: bold;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 16px;
+  }
+
+  .value {
+    font-size: 1.3rem;
+    font-weight: bold;
+    color: #1b1b1b;
+  }
+`;
+
+export const BackContent = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+`;
