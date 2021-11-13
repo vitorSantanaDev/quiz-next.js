fd342655e44ed2205a26900cd8fb23329ce3f126
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-12 22:00:38 -0300

    ✨ Introduce new features

--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -11,23 +11,39 @@ interface ResponseProps {
   index: number;
   letter: string;
   letterBkackground: string;
+  answerProvided: (index: number) => void;
 }
 
 export default function Response(props: ResponseProps) {
   const response = props.value;
   return (
-    <ResponseWrapper>
+    <ResponseWrapper onClick={() => props.answerProvided(props.index)}>
       <ContentWrapper>
-        <FrontContent>
-          <div
-            className="letter"
-            style={{ backgroundColor: props.letterBkackground }}
-          >
-            {props.letter}
-          </div>
-          <div className="value">{response.value}</div>
-        </FrontContent>
-        <BackContent></BackContent>
+        {!response.revealed ? (
+          <FrontContent>
+            <div
+              className="letter"
+              style={{ backgroundColor: props.letterBkackground }}
+            >
+              {props.letter}
+            </div>
+            <div className="value">{response.value}</div>
+          </FrontContent>
+        ) : (
+          <BackContent>
+            {response.certain ? (
+              <div className="certain">
+                <div>A resposta correta é...</div>
+                <div className="value">{response.value}</div>
+              </div>
+            ) : (
+              <div className="wrong">
+                <div>A resposta informada está incorreta...</div>
+                <div className="value">{response.value}</div>
+              </div>
+            )}
+          </BackContent>
+        )}
       </ContentWrapper>
     </ResponseWrapper>
   );
